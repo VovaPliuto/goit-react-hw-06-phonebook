e98ed7e9e2bb5ca0acabdbc9a951435945a96a33
VovaPliuto <vovaplyuto@gmail.com>
2023-07-28 21:44:16 +0300

    add src folder

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,51 @@
+import { useEffect, useState } from 'react';
+
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+import css from './App.module.css';
+
 export const App = () => {
+  const [contacts, setContacts] = useState(() => {
+    const parsedContacts = JSON.parse(localStorage.getItem('phonebook'));
+    return parsedContacts ?? [];
+  });
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('phonebook', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const onAddContact = newContact => {
+    if (contacts.find(el => el.name === newContact.name)) {
+      return alert(`${newContact.name} is already in contacts`);
+    }
+
+    setContacts(prevState => [...prevState, newContact]);
+  };
+
+  const onContactDelete = contactId => {
+    setContacts(prevState =>
+      prevState.filter(contact => contact.id !== contactId)
+    );
+  };
+
+  const handleFilterChange = e => {
+    setFilter(e.target.value);
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.contactSection}>
+      <h1>Phonebook</h1>
+      <ContactForm onAddContact={onAddContact} />
+
+      <h2>Contacts</h2>
+      <Filter onFilterChange={handleFilterChange} filterValue={filter} />
+      <ContactList
+        data={contacts}
+        onContactDelete={onContactDelete}
+        filter={filter}
+      />
     </div>
   );
 };
